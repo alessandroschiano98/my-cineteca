f8bacf381b147945716acb24fbb4a697fef5b56f
alessandroschiano98 <schianoski98@gmail.com>
2025-12-01 14:43:15 +0100

    fix: double film

--- src/data/Drama.jsx
+++ src/data/Drama.jsx
@@ -40,184 +40,176 @@ export const drama = {
             poster: "https://pad.mymovies.it/filmclub/2006/02/413/locandina.jpg",
             actors: ["Kevin Bacon", "Brad Pitt", "Robert De Niro"],
             director: "Barry Levinson"
-          },
-          {
+        },
+        {
             title: "The Truman Show",
             slug: "drama-the-truman-show-1998",
             year: 1998,
             poster: "https://pad.mymovies.it/filmclub/2003/11/506/immpg2.jpg",
             actors: ["Jim Carrey", "Ed Harris", "Laura Linney"],
             director: "Peter Weir"
-          },
-          {
+        },
+        {
             title: "Gran Torino",
             slug: "drama-gran-torino-2008",
             year: 2008,
             poster: "https://pad.mymovies.it/filmclub/2008/10/127/locandina.jpg",
             actors: ["Clint Eastwood", "Bee Vang", "Ahney Her"],
             director: "Clint Eastwood"
-          },
-          {
+        },
+        {
             title: "Boogie Nights - L'altra Hollywood",
             slug: "drama-boogie-nights-1997",
             year: 1997,
             poster: "https://pad.mymovies.it/filmclub/2006/05/276/locandina.jpg",
             actors: ["Mark Wahlberg", "Julianne Moore", "Burt Reynolds"],
             director: "Paul Thomas Anderson"
-          },
-          {
-            title: "Requiem for a Dream",
-            slug: "drama-requiem-for-a-dream-2000",
-            year: 2000,
-            poster: "https://pad.mymovies.it/filmclub/2004/02/503/locandina.jpg",
-            actors: ["Ellen Burstyn", "Jared Leto", "Jennifer Connelly"],
-            director: "Darren Aronofsky"
-          },
-          {
+        },
+        {
             title: "Mystic River",
             slug: "drama-mystic-river-2003",
             year: 2003,
             poster: "https://pad.mymovies.it/filmclub/2003/05/035/locandina.jpg",
             actors: ["Sean Penn", "Tim Robbins", "Kevin Bacon"],
             director: "Clint Eastwood"
-          },
-          {
+        },
+        {
             title: "Frankenstein",
             slug: "drama-frankenstein-2025",
             year: 2025,
             poster: "https://pad.mymovies.it/filmclub/2025/06/001/locandinapg2.jpg",
             actors: ["Jacob Elordi", "Mia Goth", "Oscar Isaac"],
             director: "Guillermo del Toro"
-          },
-          {
+        },
+        {
             title: "Collateral Beauty",
             slug: "drama-collateral-beauty-2016",
             year: 2016,
             poster: "https://pad.mymovies.it/filmclub/2016/09/028/locandina.jpg",
             actors: ["Will Smith", "Edward Norton", "Kate Winslet"],
             director: "David Frankel"
-          },
-          {
+        },
+        {
             title: "The Basketball Diaries",
             slug: "drama-the-basketball-diaries-1995",
             year: 1995,
             poster: "https://pad.mymovies.it/filmclub/2007/21/539/locandina.jpg",
             actors: ["Leonardo DiCaprio", "Mark Wahlberg", "Lorraine Bracco"],
             director: "Scott Kalvert"
-          },
-          {
+        },
+        {
             title: "Buon compleanno Mr. Grape",
             slug: "drama-buon-compleanno-mr-grape-1993",
             year: 1993,
             poster: "https://pad.mymovies.it/filmclub/2001/04/056/locandinapg1.jpg",
             actors: ["Johnny Depp", "Leonardo DiCaprio", "Juliette Lewis"],
             director: "Lasse Hallström"
-          },
-          {
+        },
+        {
             title: "The Beach",
             slug: "drama-the-beach-2000",
             year: 2000,
             poster: "https://mr.comingsoon.it/imgdb/locandine/235x336/3092.jpg",
             actors: ["Leonardo DiCaprio", "Virginie Ledoyen", "Guillaume Canet"],
             director: "Danny Boyle"
-          },
-          {
+        },
+        {
             title: "La maschera di ferro",
             slug: "drama-la-maschera-di-ferro-1998",
             year: 1998,
             poster: "https://pad.mymovies.it/filmclub/2001/10/007/locandina.jpg",
             actors: ["Leonardo DiCaprio", "Jeremy Irons", "John Malkovich"],
             director: "Randall Wallace"
-          },
-          {
+        },
+        {
             title: "C'era una volta a... Hollywood",
             slug: "drama-hollywood-2019",
             year: 2019,
             poster: "https://pad.mymovies.it/filmclub/2018/06/020/locandina.jpg",
             actors: ["Leonardo DiCaprio", "Brad Pitt", "Margot Robbie"],
             director: "Quentin Tarantino"
-          },
-          {
+        },
+        {
             title: "Joker",
             slug: "drama-joker-2019",
             year: 2019,
             poster: "https://pad.mymovies.it/filmclub/2018/06/066/locandina.jpg",
             actors: ["Joaquin Phoenix", "Robert De Niro", "Zazie Beetz"],
             director: "Todd Phillips"
-          },
-          {
+        },
+        {
             title: "A Beautiful Mind",
             slug: "drama-a-beautiful-mind-2001",
             year: 2001,
             poster: "https://pad.mymovies.it/filmclub/2002/02/014/locandina.jpg",
             actors: ["Russell Crowe", "Jennifer Connelly", "Ed Harris"],
             director: "Ron Howard"
-          },
-          {
+        },
+        {
             title: "Patch Adams",
             slug: "drama-patch-adams-1998",
             year: 1998,
             poster: "https://pad.mymovies.it/filmclub/2007/01/164/locandina.jpg",
             actors: ["Robin Williams", "Monica Potter", "Philip Seymour Hoffman"],
             director: "Tom Shadyac"
-          },
-          {
+        },
+        {
             title: "Million Dollar Baby",
             slug: "drama-million-dollar-baby-2004",
             year: 2004,
             poster: "https://pad.mymovies.it/filmclub/2005/02/043/locandina.jpg",
             actors: ["Clint Eastwood", "Hilary Swank", "Morgan Freeman"],
             director: "Clint Eastwood"
-          },
-          {
+        },
+        {
             title: "The Fighter",
             slug: "drama-the-fighter-2010",
             year: 2010,
             poster: "https://pad.mymovies.it/filmclub/2009/04/165/locandina.jpg",
             actors: ["Mark Wahlberg", "Christian Bale", "Amy Adams"],
             director: "David O. Russell"
-          },
-          {
+        },
+        {
             title: "Warrior",
             slug: "drama-warrior-2011",
             year: 2011,
             poster: "https://pad.mymovies.it/filmclub/2011/01/010/locandina.jpg",
             actors: ["Tom Hardy", "Joel Edgerton", "Nick Nolte"],
             director: "Gavin O'Connor"
-          },
-          {
+        },
+        {
             title: "Non è mai troppo tardi",
             slug: "drama-non-e-mai-troppo-tardi-2007",
             year: 2007,
             poster: "https://pad.mymovies.it/filmclub/2007/12/037/locandina.jpg",
             actors: ["Morgan Freeman", "Jack Nicholson", "Sean Hayes"],
             director: "Rob Reiner"
-          },
-          {
+        },
+        {
             title: "Out of the Furnace",
             slug: "drama-out-of-the-furnace-2013",
             year: 2013,
             poster: "https://pad.mymovies.it/filmclub/2012/04/043/locandina.jpg",
             actors: ["Christian Bale", "Casey Affleck", "Woody Harrelson"],
             director: "Scott Cooper"
-          }
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-
-          
+        }
 
 
-          
-          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
