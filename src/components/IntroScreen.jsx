01f43a7503b0a96727059c9f2ddd1eacbf18b427
alessandroschiano98 <schianoski98@gmail.com>
2025-12-04 17:58:57 +0100

    fix: colors and fade effects intro  + add fonts

--- src/components/IntroScreen.jsx
+++ src/components/IntroScreen.jsx
@@ -1,26 +1,47 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "../style/IntroScreen.css";
 
-function IntroScreen() {
-  const [fadeOut, setFadeOut] = useState(false);
+export default function IntroScreen({ onFinish }) {
+    const [explode, setExplode] = useState(false);
+    const [lightOn, setLightOn] = useState(false);
+    const [hideText, setHideText] = useState(false);
+    const [flicker, setFlicker] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setFadeOut(true);
-    }, 2500);
-  }, []);
 
-  return (
-    <div className={`intro-container ${fadeOut ? "fade-out" : ""}`}>
-      <img
-        src="https://i.postimg.cc/R0kMFh5z/Logo-dell-occhio-con-raggio-di-luce.png"
-        alt="Logo"
-        className="intro-logo"
-      />
+    useEffect(() => {
+        // Accensione logo
+        setTimeout(() => setLightOn(true), 300);
 
-      <h2 className="intro-text">Non sai che film vedere..?<br />Ci penso io.</h2>
-    </div>
-  );
+        // Nascondi la scritta prima dello zoom
+        setTimeout(() => setHideText(true), 2000);
+
+        // Effetto esplosione logo
+        setTimeout(() => setExplode(true), 2600);
+
+        // Chiudi intro
+        setTimeout(() => onFinish(), 3500);
+
+        setTimeout(() => setLightOn(true), 1400); 
+
+        setTimeout(() => setFlicker(false), 1400);
+
+    }, []);
+
+    return (
+        <div className="intro-container">
+            <img
+                src="https://i.postimg.cc/R0kMFh5z/Logo-dell-occhio-con-raggio-di-luce.png"
+                alt="Logo"
+                className={`intro-logo 
+    ${flicker ? "flicker" : ""} 
+    ${lightOn ? "light-up" : ""} 
+    ${explode ? "explode" : ""}
+  `}
+            />
+
+            <h2 className={`intro-text animated-text ${hideText ? "hide-text" : ""}`}>
+                My Cineteca
+            </h2>
+        </div>
+    );
 }
-
-export default IntroScreen;
